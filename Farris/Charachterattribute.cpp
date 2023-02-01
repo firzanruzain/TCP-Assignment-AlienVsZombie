2fcebb72c87dee2aeeeb75ac7af50a1e21fa1b22
farris0406 <123624744+farris0406@users.noreply.github.com>
2023-02-01 15:23:42 +0800

    object

--- Farris/Charachterattribute.cpp
+++ Farris/Charachterattribute.cpp
@@ -1,42 +1,68 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <cstdlib> // for system()
+#include <ctime>   // for time() in srand( time(NULL) );
+#include <iomanip> // for setw()
 
-class Character {
+using namespace std;
+class Mars
+{
+private:
+  vector<vector<char>> map_; // convention to put trailing underscore
+  int dimX_, dimY_;          // to indicate private data
 public:
-  Character() {
-    x = 0;
-    y = 0;
-    dx = 0;
-    dy = 0;
-    health = 100;
-    strength = 10;
-    defense = 5;
-    level = 1;
-    experience = 0;
-    facing_right = true;
-    animation_state = 0;
+  Mars(int dimX = 15, int dimY = 5);
+  void init(int dimX, int dimY);
+  void display() const;
+};
+Mars::Mars(int dimX, int dimY)
+{
+  init(dimX, dimY);
+}
+void Mars::init(int dimX, int dimY)
+{
+  dimX_ = dimX;
+  dimY_ = dimY;
+  char objects[] = {' ', ' ', ' ', ' ', ' ', ' ', 'r', 'h', 'p',};
+  int noOfObjects = 10; // number of objects in the objects array
+  // create dynamic 2D array using vector
+  map_.resize(dimY_); // create empty rows
+  for (int i = 0; i < dimY_; ++i)
+  {
+    map_[i].resize(dimX_); // resize each row
   }
-
-  int x, y;
-  int dx, dy;
-  int health;
-  int strength;
-  int defense;
-  int level;
-  int experience;
-  bool facing_right;
-  int animation_state;
-  std::vector<std::string> inventory;
-
-  void addItem(std::string item) {
-    inventory.push_back(item);
-  }
-
-  void removeItem(std::string item) {
-    auto it = std::find(inventory.begin(), inventory.end(), item);
-    if (it != inventory.end()) {
-      inventory.erase(it);
+  // put random characters into the vector array
+  for (int i = 0; i < dimY_; ++i)
+  {
+    for (int j = 0; j < dimX_; ++j)
+    {
+      int objNo = rand() % noOfObjects;
+      map_[i][j] = objects[objNo];
     }
   }
-};
+}
+void Mars::display() const
+{
+  // for each row
+  for (int i = 0; i < dimY_; ++i)
+  {
+    // for each column
+    for (int j = 0; j < dimX_; ++j)
+    {
+      cout << map_[i][j];
+    }
+    cout << endl;
+  }
+}
+void test1_1()
+{
+  Mars mars;
+  mars.display();
+}
+int main()
+{
+  srand(1); // use this for fixed map during testing
+  // srand(time(NULL)); // try this for random map
+  test1_1();
+}
